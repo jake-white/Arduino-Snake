a8ae722d6dfacb025e9ea76219b6a65b9508252f
Jake White <squirrelboy1225@gmail.com>
2015-05-27 14:52:47 -0400

    it works! :D
    Final(?) version.  Added death lights.

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,8 +1,10 @@
-
+  bool advanceTail=true;
   bool gridArray[6][6];
   int gridNextX[6][6];
   int gridNextY[6][6];
-  int lastTime, rightButton = 11, downButton = 9, leftButton = 10, upButton = 12, LSB = 8, MSB = 7, HSB = 6;
+  int rightButton = 11, downButton = 9, leftButton = 10, upButton = 12, LSB = 8, MSB = 7, HSB = 6;
+  unsigned long lastTime,tickTime=500;
+  bool dead=false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -31,34 +33,40 @@ void setup() {
     gridNextX[i][3]=1;
     
   }
+  generatePoint();
 }
 int snakeHeadX=3,snakeHeadY=3;
 int snakeTailX=1,snakeTailY=3;
 int snakeLength = 3, foodSince;
-int flash = 0, tickTime = 1000;
+int flash = 0;
 int sizeX = 6, sizeY = 6, foodX, foodY, xDirection = 1, yDirection = 0;
 int direction=0;
 bool alive = true;
 bool killTail = true;
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // kput your main code here, to run repeatedly:
   if (flash < 7)
     ++flash;
   else
     flash = 0;
-  if (millis() - lastTime > tickTime)
+  if (millis() >lastTime + tickTime)
   {
     tick();
   }
   listenForInputs();
   //  if(alive)
   output();
+  random(4);
   delay(3);
 }
 
 void output()
 {
+  bool didThing=millis()%1000>300;
+  if(didThing){
+    gridArray[foodX][foodY]=true;
+  }
   switch (flash)
   {
     case 0:
@@ -79,20 +87,22 @@ void output()
       break;
     case 4:
       flashClock(false, false, true);
-      outputPart(gridArray[1][5], gridArray[1][3], gridArray[1][1], getFoodLEDs(0), gridArray[4][2]);
+      outputPart(gridArray[1][5], gridArray[1][3], gridArray[1][1], getDeath(0), gridArray[4][2]);
       break;
     case 5:
       flashClock(true, false, true);
-      outputPart(gridArray[3][5], gridArray[3][3], gridArray[3][1], getFoodLEDs(1), gridArray[5][2]);
+      outputPart(gridArray[3][5], gridArray[3][3], gridArray[3][1], getDeath(1), gridArray[5][2]);
       break;
     case 6:
       flashClock(false, true, true);
-      outputPart(gridArray[0][5], gridArray[0][3], gridArray[0][1], getFoodLEDs(2), gridArray[4][3]);
+      outputPart(gridArray[0][5], gridArray[0][3], gridArray[0][1], getDeath(2), gridArray[4][3]);
       break;
     case 7:
       flashClock(true, true, true);
-      outputPart(gridArray[2][5], gridArray[2][3], gridArray[2][1], getFoodLEDs(3), gridArray[5][3]);
+      outputPart(gridArray[2][5], gridArray[2][3], gridArray[2][1], getDeath(3), gridArray[5][3]);
       break;
+  }if(didThing){
+    gridArray[foodX][foodY]=false;
   }
 }
 void listenForInputs(){
@@ -154,6 +164,7 @@ void tick()
   if (alive)
   {
     advanceSnake();
+    checkFood();
   }
   lastTime = millis();
   // Serial.print("[");
@@ -163,6 +174,15 @@ void tick()
   //  }
   //  Serial.print("]");
   //  Serial.println();
+}
+
+void checkFood()
+{
+  if(gridArray[foodX][foodY]){
+    generatePoint();
+  advanceTail=false;
+  }
+   
 }
 
 void getInputs()
@@ -187,35 +207,86 @@ void getInputs()
       break;
   }
 }
-
+void generatePoint()
+ {
+  bool doContinue = true;
+  int randomX,randomY;
+  while(doContinue){
+    randomX = random(0, sizeX); randomY = random(0, sizeY);
+    doContinue=gridArray[randomX][randomY];
+  }    
+  foodX = randomX;
+  foodY = randomY;  
+}
+int animation = 0;
+int interval = 10;
+bool getDeath(int num)
+{
+  if(!dead)
+    return false;
+  if(animation < interval*4)
+  ++animation;
+  else
+  animation = 0;
+  switch(num)
+  {
+    case 0:
+    if(animation >= 0 && animation < interval)
+      return true;
+    break;
+    case 1:
+    if(animation >= interval && animation < interval*2)
+      return true;
+    break;
+    case 2:
+    if(animation >= interval*2 && animation < interval*3)
+      return true;
+    break;
+    case 3:
+    if(animation >= interval*3 && animation < interval*4)
+      return true;
+    break;
+  }
+  return false;
+    
+}
 void advanceSnake()
 {
+  if(!isValidPos(snakeHeadX+xDirection,snakeHeadY+yDirection)||gridArray[snakeHeadX+xDirection][snakeHeadY+yDirection]){
+    dead=true;
+  }
+  if(dead){
+    return; 
+  }
+  Serial.print(xDirection);
+  Serial.print(yDirection);
+  Serial.print(gridNextX[snakeTailX][snakeTailY]);
+  Serial.print(gridNextY[snakeTailX][snakeTailY]);
   Serial.print(snakeHeadX);
   Serial.print(snakeHeadY);
   Serial.print(snakeTailX);
   Serial.println(snakeTailY);
-  if(!isValidPos(snakeHeadX+xDirection,snakeHeadY+yDirection)){
-    return;//die logic here
-  }
+  
   //advance head
   gridArray[snakeHeadX+xDirection][snakeHeadY+yDirection]=true;
   gridNextX[snakeHeadX][snakeHeadY]=xDirection;
   gridNextY[snakeHeadX][snakeHeadY]=yDirection;
   
-  //advance tail
-  gridArray[snakeTailX][snakeTailY]=false;
-  snakeTailX+=gridNextX[snakeTailX][snakeTailY];
-  snakeTailY+=gridNextY[snakeTailX][snakeTailY];
   snakeHeadX+=xDirection;
   snakeHeadY+=yDirection;
+  //advance tail
+  if(advanceTail){
+    gridArray[snakeTailX][snakeTailY]=false;
+    int temp = snakeTailX;
+    snakeTailX+=gridNextX[snakeTailX][snakeTailY];
+    snakeTailY+=gridNextY[temp][snakeTailY];
+  }else{
+    advanceTail=true; 
+  }
   
   
 }
 
-bool getFoodLEDs(int num)
-{
-  return false;
-}
 bool isValidPos(int x,int y){
    return !(x<0||x>5||y<0||y>5); 
 }
